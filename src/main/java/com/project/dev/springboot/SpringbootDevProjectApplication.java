1591b3d657b8545879c6ce04ad9539bf3b1912d0
leejunsu <qkfhtit@gmail.com>
2021-08-12 09:58:38 +0900

    초기 파일 푸시

---
+++ src/main/java/com/project/dev/springboot/SpringbootDevProjectApplication.java
@@ -0,0 +1,13 @@
+package com.project.dev.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDevProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDevProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/dev/springboot/SpringbootDevProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.dev.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDevProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
