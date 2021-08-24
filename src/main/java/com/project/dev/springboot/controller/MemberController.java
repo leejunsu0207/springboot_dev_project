e69e5f17256cb680ec53c9998a409f881129c27d
leejunsu <qkfhtit@gmail.com>
2021-08-24 10:55:34 +0900

    date타입 처리

--- src/main/java/com/project/dev/springboot/controller/MemberController.java
+++ src/main/java/com/project/dev/springboot/controller/MemberController.java
@@ -3,11 +3,14 @@ package com.project.dev.springboot.controller;
 import com.project.dev.springboot.domain.Member;
 import com.sun.org.apache.xpath.internal.objects.XString;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Slf4j
 @Controller
@@ -20,43 +23,75 @@ public class MemberController {
         return "registerForm";
     }
 
-    @PostMapping("/register01")
-    public String register01(Member member){
-        log.info(">>>>>>>>>>>>>>>>>>>>>>> register01");
+    @GetMapping("/registerByGet01")
+    public String registerByGet01(String userId,  @DateTimeFormat(pattern = "yyyyMMdd") LocalDate dateOfBirth){
+        log.info(">>>>>>>>>>>>>>>>>>>>>>> registerByGet01");
 
-        log.info("member.getUserId() = "+member.getUserId());
-        log.info("member.getPassword() = "+member.getPassword());
-        log.info("member.getCoin() = "+member.getCoin());
-
-        return "success";
-    }
-
-    @PostMapping("/register02")
-    public String register02(Member member, int coin){
-        log.info(">>>>>>>>>>>>>>>>>>>>>>> register02");
-
-        log.info("member.getUserId() = "+member.getUserId());
-        log.info("member.getPassword() = "+member.getPassword());
-        log.info("member.getCoin() = "+member.getCoin());
-        log.info("coin = "+coin);
+        log.info("userId = "+userId);
+        log.info("dateOfBirth = "+dateOfBirth);
 
         return "success";
     }
 
-    @PostMapping("/register03")
-    public String register03(int uid, Member member){
-        log.info(">>>>>>>>>>>>>>>>>>>>>>> register03");
+    @GetMapping("/registerByGet02")
+    public String registerByGet02(Member member){
+        log.info(">>>>>>>>>>>>>>>>>>>>>>> registerByGet02");
 
-        log.info("uid = "+uid);
+        log.info("member.getUserId() = "+member.getUserId());
+        log.info("member.getDateOfBirth() = "+member.getDateOfBirth());
+
+        return "success";
+    }
+
+    @PostMapping("/register")
+    public String register(Member member){
+        log.info(">>>>>>>>>>>>>>>>>>>>>>> register");
+
         log.info("member.getUserId() = "+member.getUserId());
         log.info("member.getPassword() = "+member.getPassword());
-        log.info("member.getCoin() = "+member.getCoin());
-
+        log.info("member.getDateOfBirth() = "+member.getDateOfBirth());
         return "success";
     }
 
 
 
+
+
+
+//    @PostMapping("/register01")
+//    public String register01(Member member){
+//        log.info(">>>>>>>>>>>>>>>>>>>>>>> register01");
+//
+//        log.info("member.getUserId() = "+member.getUserId());
+//        log.info("member.getPassword() = "+member.getPassword());
+//        log.info("member.getCoin() = "+member.getCoin());
+//
+//        return "success";
+//    }
+//
+//    @PostMapping("/register02")
+//    public String register02(Member member, int coin){
+//        log.info(">>>>>>>>>>>>>>>>>>>>>>> register02");
+//
+//        log.info("member.getUserId() = "+member.getUserId());
+//        log.info("member.getPassword() = "+member.getPassword());
+//        log.info("member.getCoin() = "+member.getCoin());
+//        log.info("coin = "+coin);
+//
+//        return "success";
+//    }
+//
+//    @PostMapping("/register03")
+//    public String register03(int uid, Member member){
+//        log.info(">>>>>>>>>>>>>>>>>>>>>>> register03");
+//
+//        log.info("uid = "+uid);
+//        log.info("member.getUserId() = "+member.getUserId());
+//        log.info("member.getPassword() = "+member.getPassword());
+//        log.info("member.getCoin() = "+member.getCoin());
+//
+//        return "success";
+//    }
 
 
 
--- src/main/java/com/project/dev/springboot/domain/Member.java
+++ src/main/java/com/project/dev/springboot/domain/Member.java
@@ -1,6 +1,9 @@
 package com.project.dev.springboot.domain;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -10,6 +13,9 @@ public class Member {
     private String userId = "leejunsu";
     private String password = "password11";
     private int coin = 777;
+
+    @DateTimeFormat(pattern = "yyyyMMdd")
+    private LocalDate dateOfBirth;
 
 //    @Builder
 //    public Member(String userId, String password){
