0d40278c26cf900b9e37ef9567a4e3bddc281291
leejunsu <qkfhtit@gmail.com>
2021-09-24 10:26:59 +0900

    파일 업로드 ajax방식 요청 처리

--- src/main/java/com/project/dev/springboot/controller/MemberController.java
+++ src/main/java/com/project/dev/springboot/controller/MemberController.java
@@ -19,141 +19,158 @@ import java.util.List;
 @RestController
 public class MemberController {
 
+    // 파일업로드 Ajax방식 요청 처리
+    @PostMapping(value="/uploadAjax", produces = "text/plain;charset=UTF-8")
+    public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception{
+        String originalFilename = file.getOriginalFilename();
 
+        log.info("originalName : "+originalFilename);
 
-    // Ajax 방식 요청 처리
-    @GetMapping("/register/{userId}")
-    public ResponseEntity<String> register01(@PathVariable("userId") String userId){
-        log.info("register01");
-
-        log.info("userId = " + userId);
-
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+        ResponseEntity<String> entity = new ResponseEntity<>("upload success"+originalFilename, HttpStatus.OK);
 
         return entity;
     }
 
-    @PostMapping("/register/{userId}/{password}")
-    public ResponseEntity<String> register02(@PathVariable("userId") String userId, @PathVariable("password") String password){
-        log.info("register02");
 
-        log.info("userId = " + userId);
-        log.info("password = " + password);
 
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
 
-        return entity;
-    }
 
-    @PostMapping("/register03")
-    public ResponseEntity<String> register03(@RequestBody Member member){
-        log.info("register03");
 
-        log.info("userId = " + member.getUserId());
-        log.info("password = " + member.getPassword());
 
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
 
-        return entity;
-    }
-
-    @PostMapping("/register04")
-    public ResponseEntity<String> register04(String userId){
-        log.info("register04");
-
-        log.info("userId = " + userId);
-
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
-
-        return entity;
-    }
-
-    @PostMapping("/register05")
-    public ResponseEntity<String> register05(String userId, String password){
-        log.info("register05");
-
-        log.info("userId = " + userId);
-        log.info("password = " + password);
-
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
-
-        return entity;
-    }
-
-    @PostMapping("/register/{userId}")
-    public ResponseEntity<String> register06(@PathVariable("userId") String userId, @RequestBody Member member){
-        log.info("register06");
-
-        log.info("userId = " + userId);
-
-        log.info("member.getUserId() = " + member.getUserId());
-        log.info("member.getPassword() = " + member.getPassword());
-
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
-
-        return entity;
-    }
-
-    @PostMapping("register07")
-    public ResponseEntity<String> register07(@RequestBody List<Member> memberList){
-        log.info("register07");
-
-        for(Member member : memberList) {
-            log.info("member.getUserId() = " + member.getUserId());
-            log.info("member.getPassword() = " + member.getPassword());
-        }
-
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
-
-        return entity;
-    }
-
-    @PostMapping("register08")
-    public ResponseEntity<String> register08(@RequestBody Member member){
-        log.info("register08");
-
-        log.info("member.getUserId() = " + member.getUserId());
-        log.info("member.getPassword() = " + member.getPassword());
-
-        Address address = member.getAddress();
-
-        if(address != null){
-            log.info("address.getPostCode() = " + address.getPostCode());
-            log.info("address.getLocation() = " + address.getLocation());
-        }else{
-            log.info("address == null");
-        }
-
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
-
-        return entity;
-    }
-
-    @PostMapping("register09")
-    public ResponseEntity<String> register09(@RequestBody Member member){
-        log.info("register09");
-
-        log.info("userId = " + member.getUserId());
-        log.info("password = " + member.getPassword());
-
-        List<Card> cardList = member.getCardList();
-
-        if(cardList != null){
-           log.info("cardList.size() = "+cardList.size());
-
-           for(int i=0; i < cardList.size(); i++){
-               Card card = cardList.get(i);
-               log.info("card.getNo() = "+card.getNo());
-               log.info("card.getValidMonth() = "+card.getValidMonth());
-           }
-        }else{
-            log.info("cardList == null");
-        }
-
-        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
-
-        return entity;
-    }
+//    // Ajax 방식 요청 처리
+//    @GetMapping("/register/{userId}")
+//    public ResponseEntity<String> register01(@PathVariable("userId") String userId){
+//        log.info("register01");
+//
+//        log.info("userId = " + userId);
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("/register/{userId}/{password}")
+//    public ResponseEntity<String> register02(@PathVariable("userId") String userId, @PathVariable("password") String password){
+//        log.info("register02");
+//
+//        log.info("userId = " + userId);
+//        log.info("password = " + password);
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("/register03")
+//    public ResponseEntity<String> register03(@RequestBody Member member){
+//        log.info("register03");
+//
+//        log.info("userId = " + member.getUserId());
+//        log.info("password = " + member.getPassword());
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("/register04")
+//    public ResponseEntity<String> register04(String userId){
+//        log.info("register04");
+//
+//        log.info("userId = " + userId);
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("/register05")
+//    public ResponseEntity<String> register05(String userId, String password){
+//        log.info("register05");
+//
+//        log.info("userId = " + userId);
+//        log.info("password = " + password);
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("/register/{userId}")
+//    public ResponseEntity<String> register06(@PathVariable("userId") String userId, @RequestBody Member member){
+//        log.info("register06");
+//
+//        log.info("userId = " + userId);
+//
+//        log.info("member.getUserId() = " + member.getUserId());
+//        log.info("member.getPassword() = " + member.getPassword());
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("register07")
+//    public ResponseEntity<String> register07(@RequestBody List<Member> memberList){
+//        log.info("register07");
+//
+//        for(Member member : memberList) {
+//            log.info("member.getUserId() = " + member.getUserId());
+//            log.info("member.getPassword() = " + member.getPassword());
+//        }
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("register08")
+//    public ResponseEntity<String> register08(@RequestBody Member member){
+//        log.info("register08");
+//
+//        log.info("member.getUserId() = " + member.getUserId());
+//        log.info("member.getPassword() = " + member.getPassword());
+//
+//        Address address = member.getAddress();
+//
+//        if(address != null){
+//            log.info("address.getPostCode() = " + address.getPostCode());
+//            log.info("address.getLocation() = " + address.getLocation());
+//        }else{
+//            log.info("address == null");
+//        }
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
+//
+//    @PostMapping("register09")
+//    public ResponseEntity<String> register09(@RequestBody Member member){
+//        log.info("register09");
+//
+//        log.info("userId = " + member.getUserId());
+//        log.info("password = " + member.getPassword());
+//
+//        List<Card> cardList = member.getCardList();
+//
+//        if(cardList != null){
+//           log.info("cardList.size() = "+cardList.size());
+//
+//           for(int i=0; i < cardList.size(); i++){
+//               Card card = cardList.get(i);
+//               log.info("card.getNo() = "+card.getNo());
+//               log.info("card.getValidMonth() = "+card.getValidMonth());
+//           }
+//        }else{
+//            log.info("cardList == null");
+//        }
+//
+//        ResponseEntity<String> entity = new ResponseEntity<>("success", HttpStatus.OK);
+//
+//        return entity;
+//    }
 
 
 
